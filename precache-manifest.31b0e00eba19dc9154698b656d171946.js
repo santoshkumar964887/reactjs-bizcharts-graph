87fbbaee19ef9cf159f3c43eee8f2880c0222a4c
Santosh Kumar <santoshkumar964887@gmail.com>
2021-02-08 16:43:29 +0530

    Updates

---
+++ precache-manifest.31b0e00eba19dc9154698b656d171946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b23c6d2831a53167f7208a2b7349b9",
+    "url": "/reactjs-bizcharts-graph/index.html"
+  },
+  {
+    "revision": "6fe6a6fa5433cc87d822",
+    "url": "/reactjs-bizcharts-graph/static/css/main.90110fd3.chunk.css"
+  },
+  {
+    "revision": "0f97597c0282ba0c5278",
+    "url": "/reactjs-bizcharts-graph/static/js/2.f7f28604.chunk.js"
+  },
+  {
+    "revision": "064936ab3c14a99f51cfa757572145aa",
+    "url": "/reactjs-bizcharts-graph/static/js/2.f7f28604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe6a6fa5433cc87d822",
+    "url": "/reactjs-bizcharts-graph/static/js/main.a14711d2.chunk.js"
+  },
+  {
+    "revision": "2f14b9f02e418d0b8d04",
+    "url": "/reactjs-bizcharts-graph/static/js/runtime-main.cac1abb2.js"
+  }
+]);
